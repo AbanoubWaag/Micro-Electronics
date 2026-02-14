b0cba4ada042a68969b491ba2b90de752587eb29
AbanoubWaag <AbanoubWaag>
2026-02-14 17:41:59 +0200

    ..

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+const bcrypt = require("bcrypt");
+
 
 
 const express = require("express");
@@ -6,25 +8,103 @@ const express = require("express");
 
 const mongoose = require("mongoose");
 
+
 const app = express()
 
 app.use(express.json())
 
+
+const port = process.env.PORT || 3000;
+
 async function DBconnection(){
     try{
-        mongoose.connect(process.env.DB_URL);
+        await mongoose.connect(process.env.DB_URL);
         console.log("db connected")
     }catch(error){
         console.log("db disconnected")
     }
 }
 
-
-const port = process.env.PORT || 3000;
-
 DBconnection()
+
+const   User = require("./models/User")
+
+app.post("/register", async (req, res) => {
+    try {
+        const {username,email,password} = req.body;    
+        
+        if
+(!username || !email || !password){
+            return res.status(400).json({message:"all fields are required"}) //missing data
+        }
+
+
+
+        const existUser = await User.findOne({ email });
+
+         if (existUser) {
+            return res.status(400).json({ message: "user already exists" });
+        }
+
+        const hashPassword =await bcrypt.hash(password, 10);
+
+        const user = await  User.create({
+            username,
+            email,
+            password:hashPassword,
+            role
+            :"user"
+        })
+        res.status(201).json({
+            msg:"done created user",
+            data:user
+        });
+      
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "server error" });
+    }
+});
+
+
+
+app.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+      if(!email || !password) {
+            return res.status(400).json({ message: "all fields are required" });
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "invalid credentials" });  // or accocunt not found
+        }
+        const matchPassword = await bcrypt.compare(password, user.password);
+        if(!matchPassword) {
+            return res.status(400).json({ message: "invalid credentials" });
+        }
+
+        const authcode = Buffer.from(user._id.toString()).toString("base64");
+
+        res.status(200).json({
+            msg: "login success",
+            data: authcode
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
 
 app.get("/",(req, res)=>{
     res.send("welcome to root")
 })
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
 
---
+++ models/User.js
@@ -0,0 +1,30 @@
+//require mongoose
+const mongoose = require("mongoose")
+
+//create schema
+const UserSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        trim:
+        true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:6
+    },
+    role:{
+        type:String,
+        enum:["user","admin"],
+        default:"user"
+    }
+},{timestamps:true})
+
+//export
+module.exports = mongoose.model("User",UserSchema)
